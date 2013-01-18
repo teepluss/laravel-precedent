ff7c8a5d5319fbcf5846ae5912b9af1a53364d86
Tee <admin@jquerytips.com>
2013-01-18 23:41:57 +0700

    fixed

--- model.php
+++ model.php
@@ -1,6 +1,15 @@
 <?php
 
+use Laravel\Messages;
+
 abstract class Precedent extends Eloquent {
+
+	 /**
+	* Detective Errors
+	*
+	* @var Laravel\Messages $errors
+	*/
+	public $errors;
 
 	/**
 	* Enable Cache
@@ -8,16 +17,16 @@ abstract class Precedent extends Eloquent {
 	* @var bool $cache
 	*/
 	public static $cache = false;
-	
+
 	/**
-	 * Cache driver 
+	 * Cache driver
 	 *
 	 * For nommaly the driver is inherit from config.
 	 *
 	 * @var string $cache_driver
 	 */
 	public static $cache_driver = null;
-	
+
 	/**
 	* Cache live in minutes
 	*
@@ -26,23 +35,51 @@ abstract class Precedent extends Eloquent {
 	public static $cache_ttl = 15;
 
    /**
-	* Detective Caches 
+	* Precedent Caches
 	*
 	* @var array $object_cached
 	*/
 	public static $object_cached = array();
-	
-	/** 
+
+	/**
+	* Precedent Validation Rules
+	*
+	* @var array $rules
+	*/
+	public static $rules = array();
+
+	/**
+	* Precedent Validation Messages
+	*
+	* @var array $messages
+	*/
+	public static $messages = array();
+
+	/**
+	* Create new Precedent instance
+	*
+	* @param  array $attributes
+	* @return void
+	*/
+	public function __construct($attributes = array(), $exists = false)
+	{
+		// initialize empty messages object
+		$this->errors = new Messages();
+
+		parent::__construct($attributes, $exists);
+	}
+
+	/**
 	 * Get uniqe key
 	 *
-	 * @param  integer
-	 * @param  string
+	 * @param  integer $id
+	 * @param  string  $array
 	 */
-	public function key_cache($id, $array = array())
-	{		
+	public function key_cache($id)
+	{
 		return Str::lower(get_class($this)).'_'.$id;
 	}
-	
+
 	/**
 	 * Find a model by its primary key with improve cache in object array.
 	 *
@@ -50,34 +87,35 @@ abstract class Precedent extends Eloquent {
 	 * @param  array   $columns
 	 * @return Model
 	 */
-	public function _find($id, $columns = array('*'))
-	{		
-		$ckey = $this->key_cache($id, $columns);
-		
+	public static function find($id)
+	{
+		$model = new static(array(), true);
+
+		$ckey = $model->key_cache($id);
+
+		// Object cache on every request even not cache.
 		if ( ! $result = array_get(static::$object_cached, $ckey))
-		{		
+		{
 			if (static::$cache === true)
 			{
 				if ( ! $result = Cache::get($ckey))
 				{
-					$result = $this->query()->where(static::$key, '=', $id)->first($columns);
-					if ($result)
-					{
-						Cache::put($ckey, $result, static::$cache_ttl);
-					}
-				}			
+					$result = $model->query()->where(static::$key, '=', $id)->first();
+
+					Cache::put($ckey, $result, static::$cache_ttl);
+				}
 			}
 			else
 			{
-				$result = $this->query()->where(static::$key, '=', $id)->first($columns);
+				$result = $model->query()->where(static::$key, '=', $id)->first();
 			}
-			
+
 			array_set(static::$object_cached, $ckey, $result);
-		}	
-		
+		}
+
 		return $result;
 	}
-	
+
 	/**
 	 * Override fire_event to remove cache, if cache enabled.
 	 *
@@ -87,7 +125,7 @@ abstract class Precedent extends Eloquent {
 	protected function fire_event($event)
 	{
 		parent::fire_event($event);
-		
+
 		// if cache enabled
 		if (static::$cache === true)
 		{
@@ -95,11 +133,111 @@ abstract class Precedent extends Eloquent {
 			if (in_array($event, array('updated', 'saved', 'deleted')))
 			{
 				$ckey = $this->key_cache($this->id);
-				
+
 				// remove exists cache
 				Cache::forget($ckey);
 			}
-		}		
+		}
 	}
-		
+
+	/**
+	* Validate the Model
+	*    runs the validator and binds any errors to the model
+	*
+	* @param  array  $rules
+	* @param  array  $messages
+	* @return bool
+	*/
+	public function valid($data = array(), $withs = array(), $rules = array(), $messages = array())
+	{
+		$valid = true;
+
+		if ( ! empty($rules) || ! empty(static::$rules))
+		{
+			// If empty rules, so get from static.
+			if (empty($rules))
+			{
+				$rules = static::$rules;
+
+				// Merge validation rules from related.
+				if (count($withs)) foreach ($withs as $with)
+				{
+					if (class_exists($with))
+					{
+						$rules = array_merge($rules, $with::$rules);
+					}
+				}
+			}
+
+			// If empty messages, so get from static.
+			if (empty($messages))
+			{
+				$messages = static::$messages;
+
+				// Merge validation messages from related.
+				if (count($withs)) foreach ($withs as $with)
+				{
+					if (class_exists($with))
+					{
+						$rules = array_merge($rules, $with::$messages);
+					}
+				}
+			}
+
+			// If the model exists, this is an update.
+			if ($this->exists)
+			{
+
+				$_data = array();
+				foreach ($data as $key => $value)
+				{
+				    if ( ! array_key_exists($key, $this->original) or $value != $this->original[$key])
+				    {
+				        $_data[$key] = $value;
+				    }
+				}
+
+				// Then just validate the fields that are being updated.
+				$rules = array_intersect_key($rules, $_data);
+			}
+
+
+			// Construct the validator
+			$validator = Validator::make($data, $rules, $messages);
+
+			// Validate.
+			$valid = $validator->valid();
+
+			// If the model is valid, unset old errors.
+			// Otherwise set the new ones.
+			if ($valid)
+			{
+				$this->errors->messages = array();
+			}
+			else
+			{
+				$this->errors = $validator->errors;
+			}
+		}
+
+		return $valid;
+	}
+
+	/**
+	* Magic Method for setting Precedent attributes.
+	*    ignores unchanged attibutes delegates to Eloquent
+	*
+	* @param  string  $key
+	* @param  string|num|bool|etc  $value
+	* @return void
+	*/
+	public function __set($key, $value)
+	{
+		// only update an attribute if there's a change
+		if (!array_key_exists($key, $this->attributes) || $value !== $this->$key)
+		{
+			parent::__set($key, $value);
+		}
+	}
+
 }
